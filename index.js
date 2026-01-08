1a73de8a1b70d8d472b6d64d17eedcc73b10c3b7
Aman K <cse.amankumar@gmail.com>
2026-01-08 23:50:02 +0530

    removed view set

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const blogs = [];
 
-// REQUIRED
-app.set("view engine", "ejs");
-app.set("views", "./views");
+// // REQUIRED
+// app.set("view engine", "ejs");
+// app.set("views", "./views");
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
